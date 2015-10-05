ac3e2657d44295a3589b8d0078ef3bf40ab6c5f5
uvsq21506508 <mohamedzyx@gmail.com>
2015-10-05 10:48:40 +0200

    programme bonjour

--- src/main/groovy/fr/uvsq/doosa/gradletuto/Main.groovy
+++ src/main/groovy/fr/uvsq/doosa/gradletuto/Main.groovy
@@ -2,6 +2,11 @@ package fr.uvsq.doosa.gradletuto
 
 class Main {
     static void main(def args) {
-        println 'Hello UVSQ'
+        println 'Hello UVSQ '
+	println 'bonjour 10 fois'
+	def var = 'Bonjour'
+	for(int i = 0 ; i < 10; i++)
+		println var
+	
     }
 }
